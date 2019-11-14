cf5bef56147892f5ca400f8d0cd1c5af957f86df
mrahman97 <57292586+mrahman97@users.noreply.github.com>
2019-11-14 15:36:56 -0500

    Add files via upload

---
+++ src/main/java/com/piit/session/amazon/Home.java
@@ -0,0 +1,41 @@
+package com.piit.session.amazon;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Home {
+	WebDriver driver;
+@FindBy(id="homeval")
+WebElement homevaluetext;
+@FindBy(id="downpayment")
+WebElement downpaymentoption;
+@FindBy(id="loanamt")
+WebElement loanfl;
+@FindBy(id="intrstsrate")
+WebElement it;
+@FindBy (name="ratebutton")
+WebElement buttoncl;
+
+public Home(WebDriver driver) {
+	this.driver=driver;
+	PageFactory.initElements(driver, this);
+}
+public void homevalueinput(String digits) {
+	homevaluetext.clear();
+	homevaluetext.sendKeys(digits);
+}
+public void downpaymentinput(String ui) {
+	downpaymentoption.clear();
+	downpaymentoption.sendKeys(ui);
+}
+public void intrstsrateinput(String ow) {
+	it.clear();
+	it.sendKeys(ow);
+}
+public void buttonclinput() {
+	buttoncl.click();
+}
+
+}
---
+++ src/test/java/com/piit/session/amazon/NewTest.java
@@ -0,0 +1,57 @@
+package com.piit.session.amazon;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.rmi.server.UID;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+	WebDriver driver;
+
+  @Test
+  public void f() throws InterruptedException {
+	  Home ui=new Home(driver);
+	  ui.homevalueinput("800000");
+	  Thread.sleep(3000); 
+	  ui.downpaymentinput("80000");
+	  Thread.sleep(3000);
+	  ui.intrstsrateinput("28.0");
+	  Thread.sleep(3000);
+	  ui.buttonclinput();
+	  
+  }
+  
+  @BeforeClass
+ // @Parameters("browser")
+  public void beforeClass() {
+	 /* if(browsers.equalsIgnoreCase("chrome")) {
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\eclipse-workspace\\Automation_aps_code\\src\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		//WebDriver driver=new WebDriver;
+	  driver = new ChromeDriver();
+		 driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+		 driver.navigate().to("https://www.mortgagecalculator.org");
+	  }else if (browsers.equalsIgnoreCase("firefox")){
+		  System.setProperty("webdriver.gecko.driver","C:\\Users\\Admin\\eclipse-workspace\\Automation_aps_code\\src\\Drivers\\chromedriver_win32\\geckodriver.exe");
+		 driver = new FirefoxDriver();
+		 driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+		 driver.navigate().to("https://www.mortgagecalculator.org");
+  } else 
+	System.out.println("Correct to digits");
+	  }*/
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\eclipse-workspace\\Automation_aps_code\\src\\Drivers\\chromedriver_win32\\chromedriver.exe");
+	
+	  driver = new ChromeDriver();
+	  driver.navigate().to("https://www.mortgagecalculator.org");
+  }
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
